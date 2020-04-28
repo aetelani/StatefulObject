521d763ad630a5c119a556673d0553d43225f961
Anssi Eteläniemi <aetelani@hotmail.com>
2020-04-28 18:10:57 +0300

    Added more state changes

--- state_machine/test_Stateful.py
+++ state_machine/test_Stateful.py
@@ -18,15 +18,16 @@ conditions = [[lambda v: False] * len(TestStates)] * len(TestStates)
 class TestTransitions(TestCase):
     def test_init(self):
         t1: Transition = (0, 1, lambda v: 'ev1' in v, lambda v: v, lambda v: print('done', v))
-        t2, t3 = (1, 2, lambda: True, None, lambda: print('done')), \
-                 (2, 3, lambda: True, lambda: True, lambda: print('done'))
+        t2, t3 = (1, 2, lambda v: True, lambda v: v, lambda v: print('done', v)), \
+                 (2, 3, lambda v: True, lambda v: v, lambda v: print('done', v))
 
         ts = Transitions([t1, t2, t3])
         g = stateful_generator(0, ts)
         print(r := next(g))
         ev: Event = ('ev1', ('arg1', 'arg2'))
         print(r := g.send(ev))
-
+        print(r := g.send(('ev2', ('arg1', 'arg2'))))
+        print(r := g.send(('ev3', ('arg1', 'arg2'))))
         import numpy as np
         print(np.array(conditions))
         print(np.array(ts))
