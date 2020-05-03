1f10031cff1b6acaab729a422350edd4884fb9ca
Anssi Eteläniemi <aetelani@hotmail.com>
2020-05-03 18:53:17 +0300

    cleanup redundant code

--- state_machine/test_Stateful.py
+++ state_machine/test_Stateful.py
@@ -18,34 +18,12 @@ class TestStates(Enum):
     Jumping = auto()
 
 
-# Awaitable alias to https://docs.python.org/3/library/typing.html#typing.Generator
-
-def action_tmpl(p):
-    print('action running', p)
-
-
-# If priority not set, using default priority := number of transitions. Lower is higher priority.
-# In case of even priority, first scheduled actions are executed first
-def pact(priority=None, action=action_tmpl):
-    return priority, action  # Action running order pact
-
-
-action_range = range(1, len(TestStates) + 1)
-transitions = [(f, t, lambda v: 'ev1' in v, pact(), lambda v: print(f'done{f}->{t}', v))
-               for f, t in sorted(set(combinations(chain(action_range, reversed(action_range)), 2)))]
-print(*transitions)
-
-
 class TestTransitions(TestCase):
     def test_init(self):
-        t1: Transition = (0, 1, lambda v: 'ev1' in v, pact, lambda v: print('done1', v))
-        t2, t3 = (1, 2, lambda v: True, pact, lambda v: print('done2', v)), \
-                 (2, 3, lambda v: True, pact, lambda v: print('done3', v))
-
-        # ts = Transitions([t1, t2, t3])
         ts = Transitions(generate_transitions_template(TestStates))
         lq = PriorityQueue()
-        g: stateful_generator = stateful_generator(TestStates.Stopped.value, ts, q=lq, exec_wait_result=True)
+        initial_value = TestStates.Stopped.value
+        g: stateful_generator = stateful_generator(initial_value, ts, q=lq, exec_wait_result=True)
         print(r := next(g))
         ev: Event = ('ev1', ('arg1', 'arg2'))
         print(r := g.send(ev))
